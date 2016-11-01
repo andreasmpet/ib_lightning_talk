1778e276796aeb54338c22f5c0cd7a7afc2a0925
Andreas Petrov <andreas.petrov@giantleap.no>
2016-11-01 22:56:42 +0100

    Defined most of the IBDudeModel. Needs to expose the resulting transforms and states based on the input states

--- IBLightningTalk/View Model/IBDudeModel.swift
+++ IBLightningTalk/View Model/IBDudeModel.swift
@@ -9,24 +9,106 @@
 import UIKit
 
 class IBDudeModel {
-    enum HandPosition {
-        case strainingHorizontally
-        case strainingVertically
-        case huggingHorizontally
+    
+    typealias Phase = Float
+    typealias SmileWidth = Float
+
+    enum HandPosition: String {
+        case up = "up"
+        case down = "down"
+        case left = "left"
+        case right = "right"
     }
     
-    enum MouthPosition {
-        case upward
-        case downward
+    enum HandState {
+        case resisting(HandPosition)
+        case hugging(HandPosition)
+        
+        var icon: UIImage? {
+            var name: String = ""
+            switch self {
+            case .resisting(let handPosition):
+                name = "resisting_\(handPosition)"
+                break
+            case .hugging(let handPosition):
+                name = "hugging_\(handPosition)"
+                break
+            }
+            
+            return UIImage(named: name)
+        }
+    }
+    
+    enum MouthState {
+        static let frownVibrateFactor: Float = 0.2
+        
+        case smile(SmileWidth)
+        case frown(Phase)
         
         var rotationAngle: Float {
             switch(self) {
-            case .upward:
+            case .smile(_):
                 return 0
-            case .downward:
-                return Float.pi
+            case .frown(let phase):
+                return Float.pi + sin(phase) * type(of:self).frownVibrateFactor
             }
         }
+        
+        var scale: CGPoint {
+            switch(self) {
+            case .smile(let smileWidth):
+                return CGPoint(x: CGFloat(smileWidth), y: 1)
+            default:
+                return CGPoint(x: 1, y: 1)
+            }
+        }
+    }
+    
+    enum Eye {
+        case left
+        case right
+    }
+    
+    enum EyeState {
+        static let eyeOffsetFactor: Float = 0.1
+        static let eyeScaleFactor: Float = 0.2
+        
+        case squint(Eye, Phase)
+        case open(Eye, Phase)
+        
+        var rotationAngle: Float {
+            switch(self) {
+            case .open(let eye, _):
+                return (Float.pi / 6) * (eye == .left ? 1 : -1)
+            case .squint(_):
+                return 0
+            }
+        }
+        
+        var scale: Float {
+            switch(self) {
+            case .squint(let eye, let phase):
+                let phaseOffset = type(of:self).eyeOffsetFactor * (eye == .left ? 1 : -1) // Don't want the eyes to scale at the same time, so let's offset them a bit
+                return 1 + (sin(phase + phaseOffset) * type(of:self).eyeScaleFactor)
+            default:
+                return 1
+            }
+        }
+    }
+    
+    private(set) var eyeStates: [EyeState]
+    private(set) var handStates: [HandState]
+    private(set) var mouthState: MouthState
+    
+    var mouthTransform: CGAffineTransform {
+        let scale = self.mouthState.scale
+        return CGAffineTransform(rotationAngle: CGFloat(self.mouthState.rotationAngle)).concatenating(CGAffineTransform(scaleX:scale.x, y: scale.y))
+    }
+    
+    init(eyeStates: [EyeState], handStates: [HandState], mouthState: MouthState) {
+        self.eyeStates = eyeStates
+        self.handStates = handStates
+        self.mouthState = mouthState
     }
     
 }
